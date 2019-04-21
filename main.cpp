23fb11a35e2dbb8cbb9edcb9f4c343553d5f2009
Yahia Bakour <yehia234@bu.edu>
2019-04-21 13:37:43 -0400

    ADDED

--- main.cpp
+++ main.cpp
@@ -174,8 +174,7 @@ void findEdges(uint8_t *pixels, uint8_t *output, int ny, int nx, int nc) {
     int valX,valY,MAG;
     //#pragma acc data copy(output[0:nx*ny]) copy(pixels[0:nx*ny*nc]) copyin(GX[0:3][0:3]) copyin(GY[0:3][0:3]) copy(valY) copy(valX) copyin(EDGE_THRESHOLD) copyin(nx) copyin(ny) copyin(nc)
     //#pragma acc parallel loop 
-    #pragma acc data copyin(pixels[0:nx*ny*nc]) copyin(GX[0:3][0:3]) copyin(GY[0:3][0:3]) 
-    #pragma acc parallel loop 
+
 
     for(int i=0; i < ny; i++)
     {
--- utilities.cpp
+++ utilities.cpp
@@ -11,10 +11,11 @@ void Grayscale(uint8_t *pixels, uint8_t *output, int ny, int nx, int nc) {
     //Calculating the grayscale in each pixel. 
     int val1,val2,val3;
     //The values of the 3 colours (R, B and G) are all the same  
-    #pragma acc data copyin(pixels[0:nx*ny*nc]) copyin(ny) copyin(nx) 
+    #pragma acc data copyin(pixels[0:nx*ny*nc]) copyin(ny) copyin(nx) copyout(output[0:nx*ny*nc])
     #pragma acc parallel loop 
     for(int i=0; i < ny; i++)
         {
+            #pragma acc loop independent 
             for(int j=0; j < nx; j++)
             {
                 val1 = pixels[yxc(i,j,0,nx,nc)];
