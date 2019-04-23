9c6551e14e9113f60f0d57a9532e68cf3223fd5a
Yahia Bakour <yehia234@bu.edu>
2019-04-23 12:28:43 -0400

    ADDED

--- main.cpp
+++ main.cpp
@@ -206,11 +206,11 @@ void findEdges(uint8_t *pixels, uint8_t *output, int ny, int nx, int nc) {
         }
     }
 
-    #pragma acc data copyout(output[0:nx*ny]) create(MAG) copyin(EDGE_THRESHOLD) copyin(nx) copyin(ny) copyin(TMPX[0:ny][0:nx]) copyin(TMPY[0:ny][0:nx])
-    #pragma acc parallel loop 
+    //#pragma acc data copyout(output[0:nx*ny]) create(MAG) copyin(EDGE_THRESHOLD) copyin(nx) copyin(ny) copyin(TMPX[0:ny][0:nx]) copyin(TMPY[0:ny][0:nx])
+    //#pragma acc parallel loop 
     for(int i=0; i < ny; i++)
     {
-        #pragma acc loop independent 
+        //#pragma acc loop independent 
         for(int j=0; j < nx; j++)
         {
             //Gradient magnitude
