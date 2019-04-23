761b2e484702a663eb5c0cc9d94b40a77de47dfc
Yahia Bakour <yehia234@bu.edu>
2019-04-22 22:20:04 -0400

    ADDED

--- utilities.cpp
+++ utilities.cpp
@@ -14,7 +14,6 @@ void Grayscale(uint8_t *pixels, uint8_t *output, int ny, int nx, int nc) {
     #pragma acc data copy(pixels[0:nx*ny*nc]) copyin(ny) copyin(nx) copyin(nc) copy(output[0:nx*ny*nc]) copyin(val1) copyin(val2) copyin(val3)
     for(int i=0; i < ny; i++)
         {
-            #pragma acc parallel loop 
             for(int j=0; j < nx; j++)
             {
                 val1 = pixels[yxc(i,j,0,nx,nc)];
