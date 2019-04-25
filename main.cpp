57d691824e941a7e9494eac5fc08674a3612b20f
Yahia Bakour <yehia234@bu.edu>
2019-04-25 15:38:57 -0400

    ADDED

--- main.cpp
+++ main.cpp
@@ -127,7 +127,6 @@ int main(int argc, char ** argv) {
 
     // Cleanup
     stbi_image_free(image);  
-    delete [] edges; 
     delete [] image_gray;
     for (int i=0;i<nlevels;++i) delete [] multiscaleEdges[i];
     delete [] multiscaleEdges; 
